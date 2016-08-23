1e29e3aeadf48307411264892c8744006fd05453
Anh Nguyen Tuan <xntanh@gmail.com>
2016-08-23 09:46:26 +0700

    add migrations

---
+++ database/migrations/2016_08_19_034157_create_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users');
+            $table->string('avatar');
+            $table->boolean('gender')->default(0);
+            $table->string('url');
+            $table->text('bio');
+            $table->string('phone_number', 15);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('profiles');
+    }
+}
